b51e3c63bc209b51ab6372b3501f48ae59359de8
FelipeMaD <felipemarinhodr@hotmail.com>
2024-06-02 11:58:15 -0300

    update detalhes produtos
    criação do caminho para direcionar aos detalhes dos produtos, com html e css

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 from django.views.generic import ListView
-from .views import ProdutosListView
+from .views import ProdutosListView, ProdutosDetailView
 
 urlpatterns = [
     path('', ProdutosListView.as_view(), name='produtos'),
+    path('<int:pk>/', ProdutosDetailView.as_view(), name='detalhes')
     ]
--- produtos/views.py
+++ produtos/views.py
@@ -2,21 +2,27 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from .models import Produtos
-from django.views.generic import ListView
-
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 class ProdutosListView(ListView):
     model = Produtos
     template_name = 'produtos.html'
     context_object_name = 'produtos'
-    '''
-    def get_queryset (self):
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
         queryset = super().get_queryset()
-        query = self.request.GET.get('')
         if query:
-            queryset = queryset.filter( title__icontains =query)
-            return queryset'''
+            queryset = queryset.filter(titulo__icontains=query)
+        return queryset
+
+
+class ProdutosDetailView(DetailView):
+    model = Produtos
+    template_name = 'detalhesProdutos.html'
+
+
 '''
 def produtos(request):
     produtos = Produtos.objects.all()
